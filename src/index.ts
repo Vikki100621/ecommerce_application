b023e457cd519a6545c0498505b6294f11b04b80
Alexey Gorbenko <a.v.gor@mail.ru>
2023-08-07 17:26:45 +0300

    init: start sprint#2 task

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import './assets/scss/style.scss';
+import img from './assets/img/pic.jpg';
+
+const wrapper: HTMLDivElement = <HTMLDivElement> document.querySelector('.wrapper');
+
+wrapper.innerHTML = `<img src=${img}>`;
 
 // eslint-disable-next-line no-console
 console.log('Hello!');
