6c1809741f66bab44c0acc149d4dc4c844b5145c
Alexey Gorbenko <a.v.gor@mail.ru>
2023-08-10 17:57:32 +0300

    feat: [RSS-ECOMM-2_09] Implement client-side validation for required fields in the registration form

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const registrationForm: HTMLFormElement = <HTMLFormElement>document.querySelecto
 function showError(msg: string): void {
   const errorBlock: HTMLElement = <HTMLElement>document.querySelector('.error');
   const errorText: HTMLParagraphElement = <HTMLParagraphElement>errorBlock.querySelector('.error__text');
-  errorText.innerHTML = msg;
+  errorText.innerHTML += msg;
   errorBlock.classList.add('error_active');
 }
 
@@ -18,47 +18,69 @@ function hideError(): void {
   }
 }
 
+function returnInputValue(id: string): string {
+  const input: HTMLInputElement = <HTMLInputElement>document.getElementById(id);
+  return input.value;
+}
+
 function checkEmail(): void {
-  const emailInput: HTMLInputElement = <HTMLInputElement>document.getElementById('email');
-  const email = emailInput.value;
+  const email: string = returnInputValue('email');
   const examplEmail = /[-.\w]+@([\w-]+\.)+[\w-]+/g;
   if (!email.match(examplEmail)) {
-    showError('Email is wrong. Correct e-mail looks like <em>test@test.test</em>');
+    showError('Email is wrong. Correct e-mail looks like <em>test@test.test</em>.<br>');
   }
 }
 
 function checkPassword(): void {
-  const passInput: HTMLInputElement = <HTMLInputElement>document.getElementById('pass');
-  const pass = passInput.value;
+  const pass: string = returnInputValue('pass');
   if (pass.length < 8) {
-    showError('Password length must be at least 8 characters.');
-  } else if (!pass.match(/[A-Z]/)) {
-    showError('Password must have at least one uppercase letter.');
-  } else if (!pass.match(/[0-9]/)) {
-    showError('Password must have at least one digit.');
+    showError('Password length must be at least 8 characters.<br>');
+  }
+  if (!pass.match(/[A-Z]/)) {
+    showError('Password must have at least one uppercase letter.<br>');
+  }
+  if (!pass.match(/[0-9]/)) {
+    showError('Password must have at least one digit.<br>');
   }
 }
 
+function checkNotOnlyChar(str: string): boolean {
+  return Boolean(str.match(/[^a-z]/i) || !str.length);
+}
+
 function checkName(): void {
-  const firstNameInput: HTMLInputElement = <HTMLInputElement>document.getElementById('fname');
-  const firstName: string = <string>firstNameInput.value;
-  const lastNameInput: HTMLInputElement = <HTMLInputElement>document.getElementById('lname');
-  const lastName: string = <string>lastNameInput.value;
-  if (firstName.match(/[^a-z]/i) || !firstName.length) {
-    showError('First name must contain at least one character and no special characters or numbers');
-  } else if (lastName.match(/[^a-z]/i) || !lastName.length) {
-    showError('Last name must contain at least one character and no special characters or numbers');
+  const firstName: string = returnInputValue('fname');
+  const lastName: string = returnInputValue('lname');
+  if (checkNotOnlyChar(firstName)) {
+    showError('First name must contain at least one character and no special characters or numbers.<br>');
+  }
+  if (checkNotOnlyChar(lastName)) {
+    showError('Last name must contain at least one character and no special characters or numbers.<br>');
   }
 }
 
 function checkAge(): void {
-  const bDateInput: HTMLInputElement = <HTMLInputElement>document.getElementById('bdate');
-  const bDateStr: string = <string>bDateInput.value;
+  const bDateStr: string = returnInputValue('bdate');
   const bDate = Date.parse(bDateStr);
   const nowDate = new Date();
   const minBDate = Date.parse(`${nowDate.getFullYear() - 13}-${nowDate.getMonth() + 1}-${nowDate.getDate()}`);
-  if ((minBDate - bDate) < 0) {
-    showError('13 is the minimum age to be registered user.');
+  if (minBDate - bDate < 0 || !bDate) {
+    showError('13 is the minimum age to be registered user.<br>');
+  }
+}
+
+function checkAddress(): void {
+  const street: string = returnInputValue('street');
+  const city: string = returnInputValue('city');
+  const postalCode: string = returnInputValue('pcode');
+  if (!(street.match(/[^a-zA-Z0-9-]/i) || street.length)) {
+    showError('Street field must contain at least one character.<br>');
+  }
+  if (checkNotOnlyChar(city)) {
+    showError('City field must contain at least one character and no special characters or numbers.<br>');
+  }
+  if (postalCode.match(/\D/) || !(postalCode.length === 5)) {
+    showError('Postal code field must follow the format for the country (e.g., 12345 for the U.S.).<br>');
   }
 }
 
@@ -69,6 +91,12 @@ function checkForm(event: Event) {
   checkPassword();
   checkName();
   checkAge();
+  checkAddress();
+  const errorPar: HTMLParagraphElement = <HTMLParagraphElement>document.querySelector('.error__text');
+  const errorText: string = <string>errorPar.textContent;
+  if (!errorText.length) {
+    console.log("it's ok");
+  }
 }
 
 registrationForm.addEventListener('submit', checkForm);
