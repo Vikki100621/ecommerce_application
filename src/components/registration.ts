6216879ca10e2d885651d7b76d54a861cd35cb7d
Alexey Gorbenko <a.v.gor@mail.ru>
2023-08-18 16:17:39 +0300

    refactor: start add registration api

--- src/components/registration.ts
+++ src/components/registration.ts
@@ -1,3 +1,10 @@
+import {
+  // ApiRoot,
+  createApiBuilderFromCtpClient,
+} from '@commercetools/platform-sdk';
+
+import ctpClient from './api/BuildClient';
+
 export default class Registration {
   main: HTMLElement;
 
@@ -245,16 +252,30 @@ export default class Registration {
     }
 
     function checkForm(evt: Event) {
+      evt.preventDefault();
       checkAll();
       const activeError = document.querySelector('.error_active');
       if (activeError !== null) {
-        evt.preventDefault();
         const errPar: HTMLParagraphElement = returnErrPar('submit');
         showError("It's at least one error on page. Change input values and try submit again.", errPar);
         setTimeout(() => hideError(errPar), 5000);
       } else {
         registrationForm.removeEventListener('focusout', checkEvtTarget);
         registrationForm.removeEventListener('submit', checkForm);
+        // Create apiRoot from the imported ClientBuilder and include your Project key
+        const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+          projectKey: 'rs-school-ecommerce-application',
+        });
+
+        // Example call to return Project information
+        // This code has the same effect as sending a GET request to the commercetools
+        // Composable Commerce API without any endpoints.
+        // const getProject = () => apiRoot.get().execute();
+        const func = () => apiRoot.customers().get().execute();
+
+        // Retrieve Project information and output the result to the log
+        // getProject().then(console.log).catch(console.error);
+        func().then(console.log).catch(console.error);
       }
     }
 
